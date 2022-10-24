d2cd5653ee697fa3e937897391dfad660de4a806
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-10-24 13:27:14 +0200

    ok

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Michel' },
+  { id: 12, name: 'Jackie' },
+  { id: 13, name: 'Bebert' },
+  { id: 14, name: 'Constantin' },
+  { id: 15, name: 'Captain capitaine' },
+  { id: 16, name: 'OLA' },
+  { id: 17, name: 'HELLO' },
+  { id: 18, name: 'COUCOU' },
+  { id: 19, name: 'SALUT' },
+  { id: 20, name: 'CAVALA' },
+];
